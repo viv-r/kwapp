5d3da6c23b5e56f964ead8ed7b66c3b7831d28ee
Vivek <vivek@vivr.local>
2017-10-10 15:11:45 -0700

    gcr.io to docker.io

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-IMAGE_TAG=gcr.io/$PROJECT/webapp:latest
-
+IMAGE_TAG=$DUSR/webapp:latest
 docker build -t $IMAGE_TAG .
-gcloud docker -- push $IMAGE_TAG
+docker login -u $DUSR -p $DKEY
+docker push $IMAGE_TAG
 sed -i.bak 's#acme-container#$IMAGE_TAG#' ./kube/*.yaml
 kubectl apply -f kube/
