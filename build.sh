a02d4c89fc2399c94804b2d73d3c32f51bb635e2
Vivek <vivek@vivr.local>
2017-10-10 15:23:01 -0700

    sed string env var typo

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ IMAGE_TAG=$DUSR/webapp:latest
 docker build -t $IMAGE_TAG .
 docker login -u $DUSR -p $DKEY
 docker push $IMAGE_TAG
-sed -i.bak 's#acme-container#'"$IMAGE_TAG"g'#' ./kube/*.yaml
+sed -i.bak 's#acme-container#'"$IMAGE_TAG"'#' ./kube/*.yaml
 kubectl apply -f kube/
