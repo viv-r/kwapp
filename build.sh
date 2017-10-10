e3a7875366ee55d9d0ab561290b61cd463d6d26d
Vivek <vivek@vivr.local>
2017-10-10 14:48:47 -0700

    jenkins -> sh

---
+++ build.sh
@@ -0,0 +1,6 @@
+IMAGE_TAG="gcr.io/${PROJECT}/webapp:latest"
+
+docker build -t ${IMAGE_TAG} .
+gcloud docker -- push ${IMAGE_TAG}
+sed -i.bak 's#acme-container#${imageTag}#' ./kube/*.yaml
+kubectl apply -f kube/
