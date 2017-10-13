4563097d9c9a6e021d4ea84474dfce0e45880747
Vivek <vivek@vivr.local>
2017-10-13 14:59:44 -0700

    cleanup

--- build.sh
+++
@@ -1,7 +0,0 @@
-# IMAGE_TAG=$DUSR/webapp:latest
-IMAGE_TAG=gcr.io/$PROJECT/webapp:latest
-docker build -t $IMAGE_TAG .
-# docker login -u $DUSR -p $DKEY
-gcloud docker -- push $IMAGE_TAG
-sed -i.bak 's#acme-container#'"$IMAGE_TAG"'#' ./kube/*.yaml
-kubectl apply -f kube/
--- server.js
+++ server.js
@@ -8,25 +8,25 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-app.prepare()
-    .then(() => {
-        createServer((req, res) => {
-            const parsedUrl = parse(req.url, true)
-            const rootStaticFiles = [
-                '/resources/',
-                '/robots.txt',
-                '/sitemap.xml',
-                '/favicon.ico'
-            ]
-            if (rootStaticFiles.filter(str => parsedUrl.pathname.includes(str)).length > 0) {
-                const path = join(__dirname, 'static', parsedUrl.pathname)
-                app.serveStatic(req, res, path)
-            } else {
-                handle(req, res, parsedUrl)
-            }
-        })
-            .listen(port, (err) => {
-                if (err) throw err
-                console.log(`> Ready on http://localhost:${port}`)
-            })
+app.prepare().then(() => {
+    createServer((req, res) => {
+        const parsedUrl = parse(req.url, true)
+        const rootStaticFiles = [
+            '/resources/',
+            '/robots.txt',
+            '/sitemap.xml',
+            '/favicon.ico'
+        ]
+        if (rootStaticFiles.filter(str => parsedUrl.pathname.includes(str)).length > 0) {
+            const path = join(__dirname, 'static', parsedUrl.pathname)
+            console.log('static: ' + path);
+            app.serveStatic(req, res, path)
+        } else {
+            console.log('request: ' + parsedUrl);
+            handle(req, res, parsedUrl)
+        }
+    }).listen(port, (err) => {
+        if (err) throw err
+        console.log(`> Ready on http://localhost:${port}`)
     })
+});
